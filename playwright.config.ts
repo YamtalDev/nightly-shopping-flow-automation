5b697c9326fadea08953e334856d6cba4e165857
YamtalDev <tal.aharon.work@gmail.com>
2024-11-18 11:33:39 +0200

    Increasing timeout for firefox test to pass

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,9 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   testDir: './src/tests',
-  timeout: 60000,
+  timeout: 80000,
   expect: {
-    timeout: 40000,
+    timeout: 60000,
   },
   use: {
     headless: true,
@@ -14,8 +14,8 @@ const config: PlaywrightTestConfig = {
     video: 'retain-on-failure',
     screenshot: 'only-on-failure',
     trace: 'retain-on-failure',
-    actionTimeout: 40000,
-    navigationTimeout: 60000,
+    actionTimeout: 60000,
+    navigationTimeout: 80000,
   },
   projects: [
     {
